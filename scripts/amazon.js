abb34558efaf83960ef2129358f33877209ff274
P_essonam <essonammaximin@gmail.com>
2023-11-20 20:38:46 +0100

    generate html for the order summary

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,6 @@
+import { cart, addToCart } from "../data/cart.js";
+import { products } from "../data/products.js";
+
 
 let productsHTML = '';
 
@@ -61,30 +64,10 @@ products.forEach(product => {
 document.querySelector('.js-products-grid').
     innerHTML = productsHTML;
 
-document.querySelectorAll('.js-add-to-cart')
-    .forEach(button => {
-        button.addEventListener('click', () => {
 
-           const productId = button.dataset.productId;
-           let matchingItem;
 
-            cart.forEach(item => {
-                if(productId === item.productId){
-                    matchingItem = item;
-                }
-            });
 
-            if (matchingItem) {
-                matchingItem.quantity += 1;
-            }else {
-
-                cart.push({
-                    productId: productId,
-                    quantity: 1,
-                   });
-
-            }
-
+function updateCartQuantity() {
             let cartQuantity = 0;
 
             cart.forEach(item => {
@@ -93,6 +76,18 @@ document.querySelectorAll('.js-add-to-cart')
 
             document.querySelector('.js-cart-quantity')
                 .innerHTML = cartQuantity;
- 
+}
+
+
+document.querySelectorAll('.js-add-to-cart')
+    .forEach(button => {
+        button.addEventListener('click', () => {
+
+           const productId = button.dataset.productId;
+
+           addToCart(productId);
+
+           updateCartQuantity();
+
         });
     });
